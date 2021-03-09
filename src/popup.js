574ae4591e0e43afb0fd86dc12eeff0cc20e0d50
Virej Dasani <dasanivirej@gmail.com>
2021-03-09 13:10:21 +0530

    Added content in the show more section

--- src/popup.js
+++ src/popup.js
@@ -11,6 +11,11 @@ window.onload = function () {
             let country = data.country
             let city = data.city
             let ip = data.query
+            let zip = data.zip
+            let isp = data.isp
+            let timezone = data.timezone
+            let lat = data.lat
+            let lon = data.lon
 
             if (status == "success") {
                 document.getElementById("location").innerHTML = country + ", " + city
@@ -20,8 +25,26 @@ window.onload = function () {
             }
             // More button onClick listener
             document.getElementById("moreButton").addEventListener('click', function () {
-                document.getElementById("more").innerHTML = country
+                // Show more data
+                document.getElementById("more").innerHTML = `
+                <h4>This data below may not be exact</h4>
+                <h2>Zip Code:</h2>
+                <h2 class="data">${zip}</h2>
+                <h2>ISP:</h2>
+                <h2 class="data">${isp}</h2>
+                <h2>Timezone:</h2>
+                <h2 class="data">${timezone}</h2>
+                <h2>Lat/Lon:</h2>
+                <h2 class="data">${lat}/${lon}</h2>
+                `
+                document.getElementById("moreButton").innerHTML = "Show less"
+                document.getElementById("moreButton").id = "hideButton"
+                
+                document.getElementById('hideButton').addEventListener('click', function () {
+                    document.getElementById('more').innerHTML = ""
+                    document.getElementById("hideButton").innerHTML = "Show more"
+                    document.getElementById("hideButton").id = "moreButton"
+                })
             })
         })
-    
 }
